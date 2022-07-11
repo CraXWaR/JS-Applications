1249b89bf4454b6fde47084310549b05d5803747
CraXWaR <craxwar@gmail.com>
2022-07-11 16:05:06 +0300

    1 to 4 (4 not done)

--- Data and Authentication - Exercise/01.Messenger/app.js
+++ Data and Authentication - Exercise/01.Messenger/app.js
@@ -1,5 +1,40 @@
 function attachEvents() {
-    console.log('TODO...');
+    //console.log('TODO...');
+    const url = 'http://localhost:3030/jsonstore/messenger';
+
+    const msgElement = document.getElementById('messages');
+    const authorElement = document.querySelector('input[name=author]');
+    const contentElement = document.querySelector('input[name=content]');
+    const submitBtn = document.getElementById('submit');
+    const refreshBtn = document.getElementById('refresh');
+
+    submitBtn.addEventListener('click', function onSend(e) {
+        const newMsg = {
+            author: authorElement.value,
+            content: contentElement.value,
+        }
+        if (authorElement.value != '' && contentElement.value != '') {
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json',
+                },
+                body: JSON.stringify(newMsg)
+            })
+                .then(() => {
+                    authorElement.value = '';
+                    contentElement.value = '';
+                });
+        }
+    });
+
+    refreshBtn.addEventListener('click', function onRefresh(e) {
+        msgElement.value = '';
+        fetch(url)
+            .then(res => res.json())
+            .then(res => Object.values(res))
+            .then(messages => msgElement.value += messages.map(msg => `${msg.author}: ${msg.content}`).join('\n'));
+    });
 }
 
 attachEvents();
